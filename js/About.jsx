6169b9ee18c0205943ff6438fbcec11a09804a2a
Toycu27 <toycu@gmx.de>
2022-03-02 08:50:14 +0100

    Update: About Page text

--- js/About.jsx
+++ js/About.jsx
@@ -8,9 +8,9 @@ function About () {
         <br />
         <h2>Notice</h2>
         <ul>
-            <li>Babbel compiles JSX Code, if {'<script type="text/babel"></script>'} Tags are provided with the type="text/babel" Attribute.</li>
+            <li>Babbel compiles JSX Code, if {'<script>'} Tags are provided with the type="text/babel" Attribute.</li>
             <li>Since Babbel compiles in the Browser, you can execute the following Code in the Browser Console: ReactDOM.render(App(), document.getElementById('root'));</li>
-            <li>Scripts MUST be loaded in the right order, because Scripts on the bottom depend on the Scripts at the top.</li>
+            <li>Be carefull when changing Script orders, because Scripts on the bottom depend on the Scripts at the top.</li>
             <li>You can use a CDN Server like <a href="https://cdnjs.com/">cdnjs.com</a> or <a href="https://www.jsdelivr.com/">jsdelivr.com</a> instead of hosting vendor files yourself.</li>
             <li>You should switch the commented Script Dependencys in the index.html {"<head>"} Tag with the uncommented ones in production mode.</li>
         </ul>
